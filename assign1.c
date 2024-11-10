244dfe75bd4c9f8341f818a11ed06a73f940cd9a
bhavanada123 <anandmurthyanand124@gmail.com>
2024-11-10 12:46:38 +0530

    Add files via upload

---
+++ assign1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a hexagonal number:");
+    scanf("%x",&num);
+
+    printf("%5d\n",num);
+    printf("%5o\n",num);
+    printf("%5x\n",num);
+    return 0;
+    
+}
