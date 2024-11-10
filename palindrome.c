247cd44df526dcba3a106a300a7b7c82818b17d9
bhavanada123 <anandmurthyanand124@gmail.com>
2024-11-10 12:45:49 +0530

    Add files via upload

--- palindrome.c
+++ palindrome.c
@@ -1,9 +1,21 @@
 #include<stdio.h>
-int isPallin(char*a);
+#include<string.h>
+int isPallin(char*);
 int main()
 {
-    int i=0,j=0;
-    char a[100];
-    printf("enter a word\n");
-    gets(a);
+    char s[100];
+    scanf("%s",s);
+    printf("%s is %s",s,isPallin(s)?"a pallindrome":"not a pallindrome");
+    return 0;
+}
+int isPallin(char*s)
+{
+    static int i=0;
+    int len=strlen(s);
+    if(i>=len/2)return 1;
+    if(s[i]!=s[len-1-i])return 0;
+    else{
+        i++;
+        return isPallin(s);
+    }
 }
